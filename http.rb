dc300a8d8aff3d7bb1c02786293d40aa9602c835
Dewanshu-Chhatlani <55981827+Dewanshu-Chhatlani@users.noreply.github.com>
2019-11-22 12:54:54 +0530

    Update http.rb

--- http.rb
+++ http.rb
@@ -10,7 +10,7 @@ body = ''
 header = '<!DOCTYPE html>
         <html lang="en">
             <head>
-                <title>CSS Template</title>
+                <title>FeedFetch</title>
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <style>
